6fc2f0a7f65f1e5089a9077b39687cd9a3597e69
Ednei R Louback <89962959+nei22@users.noreply.github.com>
2021-10-05 23:12:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,141 @@
+function botão(){
+    document.getElementById("agradecimento").innerHTML="<b>Não aconteceu nada &#128514 !!!!!</b>"
+    //alert("Obrigado por clicar")
+}
+function redirecionar(){
+    // outra opcão --> window.open("https://www.cjdinfo.com.br/utilitario-tabela-codigo-emoji");
+    window.location.href="https://www.cjdinfo.com.br/utilitario-tabela-codigo-emoji"
+}
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML="Passe o mouse aqui"
+    elemento.innerHTML="Obrigado por passar o mouse"
+}
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML="Passe o mouse aqui"
+    elemento.innerHTML="Passe o mouse aqui"
+}
+function load(){
+    alert("Pagina carregada!")
+}
+function FuncaoChange(){
+     console.log(elemento.value)
+}
+
+
+
+
+/*function botão(){
+    document.getElementById("agradecimento").innerHTML="<b>Não aconteceu nada &#128514 !!!!!</b>"
+    //alert("Obrigado por clicar")
+}
+function redirecionar(){
+    // outra opcão --> window.open("https://www.cjdinfo.com.br/utilitario-tabela-codigo-emoji");
+    window.location.href="https://www.cjdinfo.com.br/utilitario-tabela-codigo-emoji"
+}
+
+function voltar(){
+document.getElementById("mousemove").innerHTML="Passe o mouse aqui"
+}*/
+
+/**/
+
+
+
+/*var validar = 0 // validar nao e o mesmo que o validar esta dentro de uma funcao
+function validarIdade(idade){
+    var validar
+    if(idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar
+}
+prompt("Qual sua idade?")
+var idade = 18
+alert(validarIdade(idade))
+ */
+
+
+/* function setReplace(frase, nome, novo_nome){
+            return frase.replace(nome, novo_nome)
+} 
+window.alert(setReplace(" Vai Japão", "Japão", "Brasil"))
+*/
+
+/*function soma (n1, n2){
+    return n1 + n2
+}
+alert(soma (5, 10))*/
+
+/*var d = new Date
+alert(d)
+alert(d.getDate
+    (d.getDay
+    (d.getHours
+    (d.getMinutes
+    ))))*/
+
+/*var cout =0;
+    for(count=0; count<= 5; count ++){
+        alert(count);
+}*/
+
+/*var count = 5 ;
+while (count <= 5);{ // <ou <= tambem fazem diferença
+    console.log(count)
+    cout++; // Ou count + count +
+}*/
+
+/*var idade = prompt(" Qual sua idade?");
+if (idade >= 18){ // > ou >= faz diferença
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}*/
+
+
+/*var idade = 18;
+if (idade >= 18){ // > ou >= faz diferença
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}*/
+
+
+/*var n1 = 5;
+var n2 = 3;
+console.log(n1 * n2)
+*/
+
+
+/*var nome = "Brasil"
+var frase = "Japão é o melhor time do mundo.";
+console.log(nome)
+console.log(frase)
+console.log(frase.toUpperCase)
+//console.log(frase.toLowerCase)*/
+
+
+/*var nome = " ednei."
+var idade = "29";
+var idade2 = "10";
+alert(idade + idade2);*/
+
+/*var nome = " ednei."
+var idade = 29;
+var idade2 = 10;
+alert(idade + idade2);*/
+
+
+/*var nome = " ednei."
+var idade = 29;
+var idade2 = 10;
+alert(nome + " tem " + idade + " anos ");*/
+
+
+/*var nome = " ednei."
+alert("bem vindo"+ nome )*/
+
+
+//alert('my first js');//
